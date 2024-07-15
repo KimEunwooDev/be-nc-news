32c64089ef2c78c1d2b873573129013e6bbc9627
KimEunwooDev <eunwoo.k@icloud.com>
2024-07-15 16:30:23 +0100

    add GET /api endpoint and controller function(getAPi()), model function(readEndpoints())

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const fs = require("fs/promises");
 
 const { getTopics } = require("./controllers/topics-controllers");
+const { getApi } = require("./controllers/api-controllers");
+
+app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
 
