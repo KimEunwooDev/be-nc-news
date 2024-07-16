93b090d1c08e2e94411a8a27c94022588d8604a6
KimEunwooDev <eunwoo.k@icloud.com>
2024-07-16 17:16:08 +0100

    Create controller function getCommentsByArticleId() for GET api/articles/:article_id/comments

--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -1,12 +1,15 @@
 const {
   selectArticles,
   selectArticleById,
+  selectComments,
 } = require("../models/articles-models");
 
 exports.getArticles = (req, res, next) => {
-  selectArticles().then(({ rows }) => {
-    res.status(200).send({ articles: rows });
-  });
+  selectArticles()
+    .then(({ rows }) => {
+      res.status(200).send({ articles: rows });
+    })
+    .catch(next);
 };
 
 exports.getArticleById = (req, res, next) => {
@@ -14,6 +17,15 @@ exports.getArticleById = (req, res, next) => {
   selectArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.getCommentsByArticleId = (req, res, next) => {
+  const { article_id } = req.params;
+  selectComments(article_id)
+    .then((comments) => {
+      res.status(200).send({ comments });
     })
     .catch(next);
 };
