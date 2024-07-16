2e693c87299eedf816f7214c29ac868efa67420b
KimEunwooDev <eunwoo.k@icloud.com>
2024-07-16 16:08:22 +0100

    Create model function selectArticles() for GET api/articles

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,5 +1,25 @@
 const db = require("../db/connection");
 
+function selectArticles() {
+  //   return db.query("SELECT * FROM articles ORDER BY created_at;");
+  return db.query(
+    `SELECT
+    articles.article_id,
+    articles.title,
+    articles.topic,
+    articles.author,
+    articles.created_at,
+    articles.votes,
+    articles.article_img_url,
+    CAST(COUNT(comments.article_id) AS INTEGER) AS comment_count
+    FROM comments
+    RIGHT JOIN articles
+    ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY created_at DESC;`
+  );
+}
+
 function selectArticleById(article_id) {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
@@ -11,4 +31,4 @@ function selectArticleById(article_id) {
     });
 }
 
-module.exports = { selectArticleById };
+module.exports = { selectArticles, selectArticleById };
