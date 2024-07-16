9ae536b3dfb478a795af887343b27b285d98a0db
KimEunwooDev <eunwoo.k@icloud.com>
2024-07-16 12:56:23 +0100

    Create GET /api/articles/:article_id , Add error handling middleware for custom errors

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ const fs = require("fs/promises");
 
 const { getTopics } = require("./controllers/topics-controllers");
 const { getApi } = require("./controllers/api-controllers");
+const { getArticleById } = require("./controllers/articles-controllers");
 
 app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status).send(err);
+});
 
 module.exports = app;
