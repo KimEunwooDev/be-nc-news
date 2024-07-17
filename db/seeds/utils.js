50c169336e096834fd3903469938ec5e76b43460
KimEunwooDev <eunwoo.k@icloud.com>
2024-07-17 19:45:02 +0100

    create util function checkCommentExist()

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -62,3 +62,23 @@ exports.checkUserExist = (username) => {
       }
     });
 };
+
+exports.checkCommentExist = (comment_id) => {
+  return db
+    .query(
+      `
+    SELECT *
+    FROM comments
+    WHERE comment_id =$1
+    `,
+      [comment_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "comment does not exist",
+        });
+      } else return true;
+    });
+};
