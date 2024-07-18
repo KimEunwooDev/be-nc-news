e43b20a838a98f130a056737979942cf2c6ddec4
KimEunwooDev <eunwoo.k@icloud.com>
2024-07-18 10:55:39 +0100

    refactor function getArticles() to handle query

--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -7,7 +7,8 @@ const {
 } = require("../models/articles-models");
 
 exports.getArticles = (req, res, next) => {
-  selectArticles()
+  const { sort_by, order } = req.query;
+  selectArticles(sort_by, order)
     .then(({ rows }) => {
       res.status(200).send({ articles: rows });
     })
